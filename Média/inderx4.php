562d34da44656b79485689f98f3b04d489882b02
Lucas Nobre <108277668+LukasNR@users.noreply.github.com>
2022-09-16 11:34:23 -0300

    Add files via upload

---
+++ Média/inderx4.php
@@ -0,0 +1,12 @@
+<?php
+$nu1 = $_POST['n1'];
+$nu2 = $_POST['n2'];
+$nu3 = $_POST['n3'];
+$nu4 = $_POST['n4'];
+$nu5 = $_POST['n5'];
+
+$media = $nu1+$nu2+$nu3+$nu4+$nu5 / 5;
+
+echo 'A Média é: ' . $media
+
+?>
