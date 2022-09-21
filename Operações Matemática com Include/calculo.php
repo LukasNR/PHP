eb62885fe637075a9951412b05b5b64386e0f9c3
Lucas Nobre <108277668+LukasNR@users.noreply.github.com>
2022-09-21 10:52:24 -0300

    Add files via upload

---
+++ Operações Matemática com Include/calculo.php
@@ -0,0 +1,10 @@
+<?php
+
+include('functions.php');
+
+echo 'A Soma é: ' . soma(100,50) . "<br>";
+echo 'A Subtração é: ' . sub(100,50) . "<br>";
+echo 'A Multiplicação é: ' . mult(100,50) . "<br>";
+echo 'A Divisão é: ' . div(100,50) . "<br>";
+
+?>
---
+++ Operações Matemática com Include/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function soma($a,$b){
+    $total = $a + $b;
+    return $total;
+};
+
+function sub($a,$b){
+    $total = $a - $b;
+    return $total;
+};
+
+function mult($a,$b){
+    $total = $a * $b;
+    return $total;
+};
+
+function div($a,$b){
+    $total = $a / $b;
+    return $total;
+};
+
+?>
